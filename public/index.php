080f572449d551ac68ae79dfd1b662924d416cd2
Pietro Agazzi <agazzi_pietro@protonmail.com>
2023-07-31 23:40:19 +0200

    update welcome routes

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Pietroagazzi\Mechanic\DI\Container;
-use Pietroagazzi\Mechanic\Http\JsonResponse;
 use Pietroagazzi\Mechanic\Http\Request;
 use Pietroagazzi\Mechanic\Http\Response;
 use Pietroagazzi\Mechanic\Mechanic;
@@ -17,21 +16,12 @@ $container
 
 $app = new Mechanic($container);
 
-$app->get('/home', function (Request $request) {
-	return new Response($request->getQuery()['name'] ?? 'Hello World');
+$app->get('/welcome', function (Request $request) {
+	return new Response('Welcome to Mechanic!');
 });
 
-$app->get('/users', function (Request $request) {
-	return new JsonResponse($request->getBody());
+$app->get('/', function (Request $request) {
+	return new Response('', 302, ['Location' => '/welcome']);
 });
-
-$app->post('/post', function () {
-	return new Response('POST request');
-});
-
-$app->put('/put', function () {
-	return new Response('PUT request');
-});
-
 
 $app->handle();
